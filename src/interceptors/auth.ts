7b762b4d35cc52c6fc6a17f29967ac89c2f4edd0
EricWXY <ericwxy@foxmail.com>
2024-11-16 19:46:22 +0800

    refactor: 重构 getReqOptItem 方法

--- src/interceptors/auth.ts
+++ src/interceptors/auth.ts
@@ -1,21 +1,16 @@
 import type { Middleware } from 'onion-interceptor';
-import type { RequestParams } from '@/types';
 
 import { getReqOptItem } from '@/utils';
 import { useUserStore } from '@/store';
 import { assign } from 'lodash-es';
 
 export const authInterceptor: Middleware = async function (ctx, next) {
-  const [requestParams] = ctx.args! as [RequestParams];
-
-  if (!getReqOptItem(requestParams, 'authInterceptorEnabled')) {
-    return await next();
-  }
+  if (!getReqOptItem(ctx, 'authInterceptorEnabled')) return await next();
 
   const userStore = useUserStore();
   const token = userStore.getToken;
 
-  if (token && getReqOptItem(requestParams, 'withToken')) {
+  if (token && getReqOptItem(ctx, 'withToken')) {
     ctx.cfg!.headers = assign(ctx.cfg!.headers, {
       Authorization: `Bearer ${token}`,
     });
