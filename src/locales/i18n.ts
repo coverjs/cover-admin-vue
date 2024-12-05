57bf904d840134d1ac02ceb0d5511fb8d07c23a2
adekang <adekang@163.com>
2024-12-05 16:16:55 +0800

    refactor: 国际化 目录结构改造

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -11,7 +11,10 @@ export const i18n = createI18n({
   messages: {},
 });
 
-const modules = import.meta.glob('./langs/*.json');
+const modules = import.meta.glob([
+  './langs/global/**/*.json',
+  './langs/pages/**/*.json',
+]);
 
 const localesMap = loadLocalesMap(modules);
 
@@ -20,13 +23,36 @@ function loadLocalesMap(modules: Record<string, () => Promise<unknown>>) {
     string,
     () => Promise<{ default: Record<string, string> }>
   > = {};
+
   each(modules, (loadLocale, path) => {
     const key = path.match(/([\w-]*)\.(yaml|yml|json)/)?.[1];
     if (!key)
       return;
-    result[key] = loadLocale as () => Promise<{
-      default: Record<string, string>
-    }>;
+
+    if (!result[key]) {
+      // 如果没有加载函数，直接赋值
+      result[key] = async () => {
+        const defaultMessages = {};
+        const messages = await loadLocale() as unknown as { default: Record<string, string> }; ;
+        return { default: Object.assign(defaultMessages, messages.default) };
+      };
+    }
+    else {
+      // 如果有加载函数，合并加载函数
+      const previousLoader = result[key];
+      result[key] = async () => {
+        const defaultMessages = {};
+        const previousMessages = await previousLoader();
+        const currentMessages = await loadLocale() as unknown as { default: Record<string, string> };
+        return {
+          default: Object.assign(
+            defaultMessages,
+            previousMessages.default,
+            currentMessages.default
+          ),
+        };
+      };
+    }
   });
   return result;
 }
@@ -38,6 +64,7 @@ export function setI18nLang(locale: LanguagesType) {
 export async function loadI18nMsgs(lang: LanguagesType) {
   if (unref(i18n.global.locale) === lang || loadedLangs.has(lang))
     return setI18nLang(lang);
+
   const messages = await localesMap[lang]?.();
 
   if (messages?.default) {
